5c0d86dce453583f4990f7ba5ad797e4677f6442
fangpin <fpfangpin@163.com>
2018-11-07 22:21:59 +0800

    add data argument and read data in memory

--- mydataset.py
+++ mydataset.py
@@ -11,37 +11,52 @@ from PIL import Image
 
 class OmniglotTrain(Dataset):
 
-    def __init__(self, dataset, transform=None):
+    def __init__(self, dataPath, transform=None):
         super(OmniglotTrain, self).__init__()
         np.random.seed(0)
-        self.dataset = dataset
+        # self.dataset = dataset
         self.transform = transform
-        self.img1 = None
+        self.datas, self.num_classes = self.loadToMem(dataPath)
+
+    def loadToMem(self, dataPath):
+        print("begin loading training dataset to memory")
+        datas = {}
+        agrees = [0, 90, 180, 270]
+        idx = 0
+        for agree in agrees:
+            for alphaPath in os.listdir(dataPath):
+                for charPath in os.listdir(os.path.join(dataPath, alphaPath)):
+                    datas[idx] = []
+                    for samplePath in os.listdir(os.path.join(dataPath, alphaPath, charPath)):
+                        filePath = os.path.join(dataPath, alphaPath, charPath, samplePath)
+                        datas[idx].append(Image.open(filePath).convert('L'))
+                    idx += 1
+        print("finish loading training dataset to memory")
+        return datas, idx
 
     def __len__(self):
         return  21000000
 
     def __getitem__(self, index):
-        image1 = random.choice(self.dataset.imgs)
-        # get image from same class
+        # image1 = random.choice(self.dataset.imgs)
         label = None
+        img1 = None
+        img2 = None
+        # get image from same class
         if index % 2 == 1:
             label = 1.0
-            while True:
-                image2 = random.choice(self.dataset.imgs)
-                if image1[1] == image2[1]:
-                    break
+            idx1 = random.randint(0, self.num_classes - 1)
+            image1 = random.choice(self.datas[idx1])
+            image2 = random.choice(self.datas[idx1])
         # get image from different class
         else:
             label = 0.0
-            while True:
-                image2 = random.choice(self.dataset.imgs)
-                if image1[1] != image2[1]:
-                    break
-        image1 = Image.open(image1[0])
-        image2 = Image.open(image2[0])
-        image1 = image1.convert('L')
-        image2 = image2.convert('L')
+            idx1 = random.randint(0, self.num_classes - 1)
+            idx2 = random.randint(0, self.num_classes - 1)
+            while idx1 == idx2:
+                idx2 = random.randint(0, self.num_classes - 1)
+            image1 = random.choice(self.datas[idx1])
+            image2 = random.choice(self.datas[idx2])
 
         if self.transform:
             image1 = self.transform(image1)
@@ -49,16 +64,31 @@ class OmniglotTrain(Dataset):
         return image1, image2, torch.from_numpy(np.array([label], dtype=np.float32))
 
 
-
 class OmniglotTest(Dataset):
 
-    def __init__(self, dataset, transform=None, times=200, way=20):
+    def __init__(self, dataPath, transform=None, times=200, way=20):
         np.random.seed(1)
         super(OmniglotTest, self).__init__()
-        self.dataset = dataset
         self.transform = transform
         self.times = times
         self.way = way
+        self.img1 = None
+        self.c1 = None
+        self.datas, self.num_classes = self.loadToMem(dataPath)
+
+    def loadToMem(self, dataPath):
+        print("begin loading test dataset to memory")
+        datas = {}
+        idx = 0
+        for alphaPath in os.listdir(dataPath):
+            for charPath in os.listdir(os.path.join(dataPath, alphaPath)):
+                datas[idx] = []
+                for samplePath in os.listdir(os.path.join(dataPath, alphaPath, charPath)):
+                    filePath = os.path.join(dataPath, alphaPath, charPath, samplePath)
+                    datas[idx].append(Image.open(filePath).convert('L'))
+                idx += 1
+        print("finish loading test dataset to memory")
+        return datas, idx
 
     def __len__(self):
         return self.times * self.way
@@ -68,25 +98,18 @@ class OmniglotTest(Dataset):
         label = None
         # generate image pair from same class
         if idx == 0:
-            self.img1 = random.choice(self.dataset.imgs)
-            while True:
-                img2 = random.choice(self.dataset.imgs)
-                if self.img1[1] == img2[1]:
-                    break
+            self.c1 = random.randint(0, self.num_classes - 1)
+            self.img1 = random.choice(self.datas[self.c1])
+            img2 = random.choice(self.datas[self.c1])
         # generate image pair from different class
         else:
-            while True:
-                img2 = random.choice(self.dataset.imgs)
-                if self.img1[1] != img2[1]:
-                    break
-
-        img1 = Image.open(self.img1[0])
-        img2 = Image.open(img2[0])
-        img1 = img1.convert('L')
-        img2 = img2.convert('L')
+            c2 = random.randint(0, self.num_classes - 1)
+            while self.c1 == c2:
+                c2 = random.randint(0, self.num_classes - 1)
+            img2 = random.choice(self.datas[c2])
 
         if self.transform:
-            img1 = self.transform(img1)
+            img1 = self.transform(self.img1)
             img2 = self.transform(img2)
         return img1, img2
 
