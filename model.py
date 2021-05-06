5042faeebc3e01daf02b983b3500bb12b4ddfe1f
vythaihn <vythai@stanford.edu>
2021-05-05 21:03:33 -0700

    edited

--- model.py
+++ model.py
@@ -5,38 +5,50 @@ import torch.nn.functional as F
 
 class Siamese(nn.Module):
 
+    #TODO: Add Batch Norm
     def __init__(self):
         super(Siamese, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, 10),  # 64@96*96
+            nn.Conv2d(1, 64, 17),  # 64@96*96
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(2),  # 64@48*48
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, stride = 2),  # 64@48*48
             nn.Conv2d(64, 128, 7),
             nn.ReLU(),    # 128@42*42
-            nn.MaxPool2d(2),   # 128@21*21
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, stride = 2),   # 128@21*21
             nn.Conv2d(128, 128, 4),
             nn.ReLU(), # 128@18*18
-            nn.MaxPool2d(2), # 128@9*9
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2, stride = 2), # 128@9*9
             nn.Conv2d(128, 256, 4),
             nn.ReLU(),   # 256@6*6
         )
-        self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
-        self.out = nn.Linear(4096, 1)
+        self.fc1 = nn.Sequential(
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(4096, 1000),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(1000, 7))
+
+        #self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
+        #self.out = nn.Linear(4096, 1)
 
     def forward_one(self, x):
         x = self.conv(x)
         x = x.view(x.size()[0], -1)
-        x = self.liner(x)
+        x = self.fc1(x)
         return x
 
-    def forward(self, x1, x2):
+    def forward(self, x1):#, x2):
         out1 = self.forward_one(x1)
-        out2 = self.forward_one(x2)
-        dis = torch.abs(out1 - out2)
-        out = self.out(dis)
+        #out2 = self.forward_one(x2)
+        #dis = torch.abs(out1 - out2)
+        #out = self.out(dis)
         #  return self.sigmoid(out)
-        return out
-
+        return out1
 
 # for test
 if __name__ == '__main__':
