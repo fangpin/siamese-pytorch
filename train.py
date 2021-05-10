bc0bf17cc3d57e816380b88cf79b491de65fb551
vythaihn <vythai@stanford.edu>
2021-05-10 09:05:20 -0700

    s

--- train.py
+++ train.py
@@ -20,6 +20,41 @@ from model import Siamese
 import pandas as pd
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+classes = ['Surprise','Fear','Disgust','Happiness','Sadness','Anger','Neutral']
+
+
+def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.numpy())
+    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx], one_channel=True)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+                    color=("green" if preds[idx]==labels[idx].item() else "red"))
+    return fig
 
 
 def calculate_accuracy(y_pred, y):
@@ -111,6 +146,10 @@ def train(args, epoch, model, train_dataloader, val_dataloader, optimizer, crite
             writer.add_scalar('val accuracy',
                             val_acc,
                               (epoch-1) * len(train_dataloader) + batch_idx)
+            writer.add_figure('predictions vs. actuals',
+                              plot_classes_preds(model, train_inputs, train_label),
+                              global_step= (epoch - 1) * len(train_dataloader) + batch_idx)
+
             epoch_loss = 0.0
 
 
